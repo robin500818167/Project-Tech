6da359608cf838d0893300e9b06d2f9a34e74bec
Robin500818167 <robin.van.houten@hva.nl>
2022-02-15 14:06:33 +0100

    Opdracht serve week 2 🦖

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
 const express = require("express");
-
 const server = express();
+const port = 1337;
+
+server.use('/static', express.static('public'));
 
 server.get("/", (req, res) => {
   res.end("Hello World");
 });
+server.get("/about", (req, res) => {
+  res.end("About me");
+});
+server.get("*", (req, res) => {
+  res.end("404 Error - Page not found");
+});
 
-server.listen(9000, "localhost", () => {
-  console.log("Server is running on port 9000");
+server.listen(port, () => {
+  console.log("Server is running on port 1337");
 });
