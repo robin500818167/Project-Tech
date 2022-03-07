e5dd172d822b00d6c04656615a68f2886433e020
Robin500818167 <robin.van.houten@hva.nl>
2022-03-07 21:17:25 +0100

    Fixing Css 🎨

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-
 const express = require("express");
 const app = express();
 const hbs = require('express-hbs');
 const port = 1337;
 
-// Use `.hbs` for extensions and find partials in `views/partials`.
+// moet bovenaan blijven staan
+app.use('/static', express.static('static'));
+
 app.engine('hbs', hbs.express4({
   partialsDir: __dirname + '/views/partials'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-app.use('/static', express.static('public'));
-
 app.get("/", (req, res) => {
   res.render('main', {layout : 'index'});
 });
 app.get("/inlog", (req, res) => {
   res.render('inlog', {layout : 'index'});
+});
+app.post("/inlog", (req, res) => {
+  res.render('main', {layout : 'index'});
 });
 app.get("/forgotpassword", (req, res) => {
   res.render('forgotPassword', {layout : 'index'});
