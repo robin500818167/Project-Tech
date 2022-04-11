52c1f51f567cf0be51269aee476fc43e660c1d94
Robin500818167 <robin.van.houten@hva.nl>
2022-04-11 15:14:19 +0200

    fixing a1

--- server.js
+++ server.js
@@ -1,73 +1,61 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
+const http = require('http').Server(app);
 const hbs = require('express-hbs');
-const port = process.env.PORT || 1338;
+const port = process.env.PORT || 1337;
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const User = require('./modals/User');
+const io = require('socket.io')(http);
+const path = require('path');
 
 connectDB();
 
 // moet bovenaan blijven staan
 app.use('/static', express.static(path.join(__dirname, 'static')));
+
 app.engine('.hbs', hbs.express4({
   extname: '.hbs',
   defaultLayout: 'views/layouts/index',
   partialsDir: path.join(__dirname, 'views/partials'),
   layoutsDir: path.join(__dirname, 'views/layouts')
 }));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.set('view engine', '.hbs');
-app.set('views',path.join(__dirname,'views'))
-
-async function getUser(email, password) {
-  try {
-    const theUser = await User.find({"email": 'email'}).lean() 
-    if (user) {
-      if (password == user.password) {
-        return user
-      } else {
-        return 'user was not found'
-      }
-    }
-  } catch (error) {
-    throw new Error(error)
-  }
-}
-
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 app.get("/", (req, res) => {
- 
   res.render('main', {layout : 'index'});
 });
 app.get("/inlog", (req, res) => {
-  const dataObject = await getUser(req.body.email, req.body.password)
-  res.json(dataObject)
-  res.render('inlog', {layout : 'index'} )
+  res.render('inlog', {layout : 'index'});
 });
+app.get("/chat", (req, res) => {
+  res.render('chat', {layout : 'index'});
+});
+app.post("/inlog", (req, res) => {
+  const newUser = new User ({
+    name: "Robin",
+    email: "robinnikita@hotmail.com",
+    password: "Test123"
+  });
+  newUser.save();
+  res.render('main', {layout : 'index'});
+});
+//socket.io
+io.on('connection', socket => {
+	console.log('a user connected');
 
-  
-app.get("/forgotpassword", (req, res) => {
-  res.render('forgotPassword', {layout : 'index'});
-});
-app.get("/signup", (req, res) => {
-  res.render('signUp',  {layout : 'index'} /*, {
-    title: 'Registration Page',
-    name: '',
-    email: '',
-    password: ''    
-    }*/);
-});
-app.post("/signup", (req, res) => {
-  console.log(req.body);
-   res.send("recieved your request!");
-});
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
 
+	socket.on('message', message => {
+		console.log('message: ' + message);
+		//Broadcast the message to everyone
+		io.emit('message', message);
+	});
+});
 // * moet onderaan blijven van de routes
 app.get("*", (req, res) => {
   res.end("404 Error - Page not found");
 });
-
-app.listen(port);
+http.listen(port);
